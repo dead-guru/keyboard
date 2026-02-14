ec08932253e37eab67e3cc4ade3b3cb71073f516
assada <assada.ua@gmail.com>
2026-02-14 23:17:03 +0100

    init

---
+++ dead kbd/dead_kbdApp.swift
@@ -0,0 +1,17 @@
+//
+//  dead_kbdApp.swift
+//  dead kbd
+//
+//  Created by Oleksii Ilienko on 14/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct dead_kbdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
